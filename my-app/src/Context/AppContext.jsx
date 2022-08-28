f9fad94c89d99cbcbecca9dcc6c77e3a3041f089
aman-109 <aman.mate.52@gmail.com>
2022-08-28 17:46:01 +0530

    carpage left

--- my-app/src/Context/AppContext.jsx
+++ my-app/src/Context/AppContext.jsx
@@ -7,10 +7,10 @@ export const AuthContext =React.createContext()
 
 export default function AppContextProvider({children}){
         const [auth,setAuth] = useState({})
-        
+        const [carData,setCarData]=useState([])
 
     return(
-        <AuthContext.Provider value={{auth,setAuth}}>
+        <AuthContext.Provider value={{auth,setAuth,carData,setCarData}}>
             {children}
         </AuthContext.Provider>
     )
